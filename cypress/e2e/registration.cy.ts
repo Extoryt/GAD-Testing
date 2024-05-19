135e4eeb6e5d3bd42e683668d555152985fe41e1
Extoryt <extoryt@gmail.com>
2024-05-19 10:47:55 +0200

    Project

--- cypress/e2e/registration.cy.ts
+++ cypress/e2e/registration.cy.ts
@@ -1,8 +1,10 @@
 describe('Registration', () => {
-	it(`happy path, should allow people to sign up`, () => {
+	it('happy path, should allow people to sign up', () => {
 		cy.visit('http://localhost:3000/register.html');
+
 		const data = 'test';
 		const alphanumeric = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+
 		const generateEmail = () => {
 			let email = '';
 			for (let i = 0; i < 6; i++) {
@@ -11,14 +13,33 @@ describe('Registration', () => {
 			}
 			return email + '@example.com';
 		};
+
 		const email = generateEmail();
+
+		cy.intercept('POST', '/api/users').as('createUser');
+
 		cy.get('#firstname').type(data);
 		cy.get('#lastname').type(data);
 		cy.get('#email').type(email);
 		cy.get('#registerForm > :nth-child(6)').click();
 		cy.get(':nth-child(1) > :nth-child(5) > .ui-state-default').click();
 		cy.get('#password').type(data);
-		cy.get('div.input-field:nth-child(9)');
 		cy.get('#registerButton').click();
+
+		cy.wait('@createUser').its('response.statusCode').should('eq', 201);
+
+		cy.url({ timeout: 10000 }).should('include', 'http://localhost:3000/login/');
+	});
+});
+
+describe('Registration', () => {
+	it('a registration with all data blank, should not allow people to sign up', () => {
+		cy.visit('http://localhost:3000/register.html');
+
+		cy.get('#registerButton').click();
+
+		cy.wait('@createUser').its('response.statusCode').should('eq', 201);
+
+		cy.url({ timeout: 10000 }).should('include', 'http://localhost:3000/login/');
 	});
 });
